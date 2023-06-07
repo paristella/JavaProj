c68fa0c70c6f3fd7badfc7a789c736ae675ef6e2
paris_fsb743t <paris_fsb743t@LAPTOP-LOLBCB5N.attlocal.net>
2023-06-07 19:45:25 -0400

    First Commit

---
+++ src/scenario1/Test.java
@@ -0,0 +1,10 @@
+package scenario1;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Git and Hithub");
+	}
+
+}
